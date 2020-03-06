ec36fbb862d8c0d6c0e907186f20b6946e618703
alain <alain@biopack.be>
2020-03-06 22:15:26 +0100

    opening and closing the connections

--- DIAPI/Program.cs
+++ DIAPI/Program.cs
@@ -10,6 +10,26 @@ namespace DIAPI
     {
         static void Main(string[] args)
         {
+            //Retval you wil need when you call or update a value of sap
+            int RetVal = 0;
+            ServerConnections connection = new ServerConnections();
+            try
+            {
+                if (connection.Connect() == 0)
+                {
+                    Console.WriteLine("Connected to: " + connection.GetCompany().CompanyName);
+                    connection.GetCompany().Disconnect();
+                }
+                else
+                {
+                    Console.WriteLine("Error: " + connection.GetErrorCode()+ " : " + connection.GetErrorMessage());
+                }
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine(ex.Message);
+            }
+           
         }
     }
 }
--- DIAPI/ServerConnections.cs
+++ DIAPI/ServerConnections.cs
@@ -3,10 +3,56 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
 
 namespace DIAPI
 {
+    
     class ServerConnections
     {
+        //declaration of the privtes
+        private SAPbobsCOM.Company company = new SAPbobsCOM.Company();
+        private int connectionResult;
+        private int errorCode = 0;
+        private string errorMessage = "";
+
+        public int Connect() 
+        {
+            //pass the stored settings from app.config to a local variable
+            company.Server = ConfigurationManager.AppSettings["server"].ToString();
+            company.CompanyDB = ConfigurationManager.AppSettings["companydb"].ToString();
+            company.DbUserName = ConfigurationManager.AppSettings["dbuser"].ToString();
+            company.DbUserName = ConfigurationManager.AppSettings["dbpass"].ToString();
+            //change this variable to your enviromen( sql 2012 - 2016)
+            company.DbServerType = SAPbobsCOM.BoDataServerTypes.dst_MSSQL2012;
+            company.UserName = ConfigurationManager.AppSettings["user"].ToString();
+            company.Password = ConfigurationManager.AppSettings["password"].ToString();
+            //choose the languge of your sap install (this must match!)
+            company.language = SAPbobsCOM.BoSuppLangs.ln_Dutch;
+            //true = if ssl is enable and not self signed !
+            company.UseTrusted = false;
+            company.LicenseServer = ConfigurationManager.AppSettings["licenseserver"].ToString();
+
+            connectionResult = company.Connect();
+            if (connectionResult != 0) 
+            {
+                //connection has failed !
+                company.GetLastError(out errorCode, out errorMessage);
+            }
+            return connectionResult;
+        }
+        public SAPbobsCOM.Company GetCompany() 
+        {
+            return this.company;
+        }
+        public int GetErrorCode() 
+        {
+            return this.errorCode;
+        }
+        public string GetErrorMessage() 
+        {
+            return this.errorMessage;
+        }
+
     }
 }
